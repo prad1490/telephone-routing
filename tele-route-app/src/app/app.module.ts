59aa1ff8a684bf711bb009800e38cb14b4e2bf0f
prad1490 <dheep2lrn@gmail.com>
2019-04-03 11:57:59 +0200

    bug fix app module package missing

--- tele-route-app/src/app/app.module.ts
+++ tele-route-app/src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { DropdownDirective } from './shared/dropdown.directive';
-import { OperatorDetailComponent } from './find-operator/operator-detail/operator-detail.component';
-import { OperatorItemComponent } from './find-operator/operator-list/operator-item/operator-item.component';
-import { PriceConfigurationComponent } from './price-configuration/price-configuration.component';
 import { OperatorService } from './shared/operator.service';
 import { FindOperatorComponent } from './find-operator/find-operator.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { OperatorListComponent } from './find-operator/operator-list/operator-list.component';
-import { OperatorEditComponent } from './find-operator/operator-edit/operator-edit.component';
+import { PriceConfigurationComponent } from './price-configuration/price-configuration.component';
+import { OperatorListComponent } from './price-configuration/operator-list/operator-list.component';
+import { OperatorItemComponent } from './price-configuration/operator-list/operator-item/operator-item.component';
+import { OperatorDetailComponent } from './price-configuration/operator-detail/operator-detail.component';
+import { OperatorEditComponent } from './price-configuration/operator-edit/operator-edit.component';
 
 @NgModule({
   declarations: [
